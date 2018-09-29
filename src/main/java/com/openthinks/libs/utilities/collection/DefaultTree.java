1f91684aef2c831bd54b28b91efd6e88c13bac2f
Dailey Dai <dailey.yte@outlook.com>
2018-09-29 09:59:34 +0800

    update

--- src/main/java/com/openthinks/libs/utilities/collection/DefaultTree.java
+++ src/main/java/com/openthinks/libs/utilities/collection/DefaultTree.java
@@ -9,49 +9,58 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author dailey.yet@outlook.com
  *
  */
-public class NormalTree implements Tree {
-	private static final long serialVersionUID = -3623645850736542520L;
-	private TreeNode root;
+public class DefaultTree<T> implements Tree<T> {
+	  private static final long serialVersionUID = -3623645850736542520L;
+	  private TreeNode<T> root;
 
-	public NormalTree(TreeNode root) {
-		super();
-		this.root = root;
+	  public DefaultTree(TreeNode<T> root) {
+	    super();
+	    this.root = root;
+	  }
+
+	  @Override
+	  public int depth() {
+	    // TODO
+	    return 0;
+	  }
+
+	  @Override
+	  public int size() {
+	    final AtomicInteger rst = new AtomicInteger(0);
+	    traverse((node) -> {
+	      rst.incrementAndGet();
+	      return false;
+	    });
+	    return rst.get();
+	  }
+
+	  @Override
+	  public void traverse(TreeFilter<T> filter) {
+	    if (root == null)
+	      return;
+	    search(root, filter);
+	  }
+
+	  /**
+	   * 
+	   * search:递归遍历树节点,根据给定的{@link TreeFilter}来决定是否遍历结束. <br>
+	   * 
+	   * @param node {@link TreeNode} 开始遍历的节点
+	   * @param filter {@link TreeFilter}
+	   * @return true or false 是否遍历到目标节点
+	   */
+	  private boolean search(TreeNode<T> node, TreeFilter<T> filter) {
+	    if (filter.test(node)) {
+	      return true;
+	    } else {
+	      for (TreeNode<T> child : node.getChildren()) {
+	        if (search(child, filter) == true) {
+	          return true;
+	        }
+	      }
+	      return false;
+	    }
+
+	  }
+
 	}
-
-	@Override
-	public int depth() {
-		return 0;
-	}
-
-	@Override
-	public int size() {
-		final AtomicInteger rst = new AtomicInteger(0);
-		traverse((node)->{
-			rst.incrementAndGet();
-			return false;
-		});
-		return rst.get();
-	}
-
-	@Override
-	public void traverse(TreeFilter filter) {
-		if (root == null)
-			return;
-		search(root, filter);
-	}
-
-	private boolean search(TreeNode node, TreeFilter filter) {
-		if (filter.test(node)) {
-			return true;
-		} else {
-			for (TreeNode child : node.getChildren()) {
-				if (search(child, filter) == true) {
-					return true;
-				}
-			}
-			return false;
-		}
-
-	}
-
-}
